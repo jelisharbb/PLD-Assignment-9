0b5a97a1cd81f870e2e5f94a35a9f1bda7961665
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2022-02-09 19:37:56 +0800

    Imported summary, experience, and education details from JSON file.

--- PDF-Resume-Creator.py
+++ PDF-Resume-Creator.py
@@ -24,7 +24,7 @@ with open(importResume, "r") as resumeJson:
 
 pdf = FPDF('P', 'mm', 'Letter')
 pdf.add_page()
-pdf.set_auto_page_break(auto=True, margin=15)
+pdf.set_margin(12.7)
 
 def headerDetails(header):
     header.image('resume picture.jpg', 155, 10, 50.8)
@@ -35,6 +35,7 @@ def headerDetails(header):
     header.ln(25)
 
 def personalDetails(personal):
+    personal.set_font('helvetica', 'B', 20)
     personal.cell(90, 10, "Personal Information", ln=1, align='c')
     personal.ln(3)
     personal.set_font('helvetica', '', 12)
@@ -44,14 +45,43 @@ def personalDetails(personal):
     personal.cell(40, 6, "Address: " + str(resumeData["Personal Information"][0]["Address"]), ln=10)
     personal.cell(40, 6, "Contact Number: " + str(resumeData["Personal Information"][0]["Contact Number"]), ln=10)
     personal.cell(40, 6, "Email: " + str(resumeData["Personal Information"][0]["Email Address"]), ln=10)
+    personal.ln(3)
 
 def summaryDetails(summary):
+    summary.set_font('helvetica', 'B', 20)
     summary.cell(90, 10, "Summary", ln=1, align='c')
     summary.ln(3)
     summary.set_font('helvetica', '', 12)
+    summary.cell(40, 6, '' + str(resumeData["Summary"][0]), ln=10)
+    summary.ln(3)
+
+def experienceDetails(exp):
+    exp.set_font('helvetica', 'B', 20)
+    exp.cell(90, 10, "Experience", ln=1, align='c')
+    exp.ln(3)
+    exp.set_font('helvetica', '', 12)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Former Work"]), ln=10)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Former Company"]), ln=10)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Former Description"]), ln=10)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Latter Work"]), ln=10)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Latter Company"]), ln=10)
+    exp.cell(40, 6, '' + str(resumeData["Experience"][0]["Latter Description"]), ln=10)
+    exp.ln(3)
+
+def educationDetails(educ):
+    educ.set_font('helvetica', 'B', 20)
+    educ.cell(90, 10, "Education", ln=1, align='c')
+    educ.ln(3)
+    educ.set_font('helvetica', '', 12)
+    educ.cell(40, 6, '' + str(resumeData["Education"][0]["Course"]), ln=10)
+    educ.cell(40, 6, '' + str(resumeData["Education"][0]["University"]), ln=10)
+    educ.cell(40, 6, '' + str(resumeData["Education"][0]["Address"]), ln=10)
+    educ.ln(3)
 
 headerDetails(pdf)
 personalDetails(pdf)
+summaryDetails(pdf)
+experienceDetails(pdf)
 
-# producing pdf
+# making pdf
 pdf.output('BUGNON_JELISHARUTH.pdf')
